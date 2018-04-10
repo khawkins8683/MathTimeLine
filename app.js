01e6530a5166b9589084e01d01c598350c4fe08a
Kyle Hawkins <ksolohawk@gmail.com>
2018-04-10 12:56:38 -0700

    added code for dynamically making a tl

--- app.js
+++ app.js
@@ -1,12 +1,119 @@
-function showBottom() {
-    var x = document.getElementById("myDIV");
-    if (x.style.display === "none") {
-        x.style.display = "block";
-    } else {
-        x.style.display = "none";
-    }
+function evenQ(number){
+    return (number % 2) === 0;
 }
 
+function createTimeline(className,yearArray,descriptionArray){
+	
+	if(yearArray.length != descriptionArray.length){
+		alert("year array and descriptionArray are different lengths abortin...");
+		return;
+	}
+
+	//first, get tlSection and new conatiner for out time line
+	var tlSection = document.getElementById("timelinesection");
+	var newTLContainer = document.createElement("div");
+
+	//configure container
+	newTLContainer.classList.add("tlct");
+	newTLContainer.classList.add(className+"ct");
+
+	//now create tl display and add to container
+	var tlbkgrd = document.createElement("div");
+	tlbkgrd.classList.add("tl");
+	tlbkgrd.classList.add("bkgrd");
+	tlbkgrd.classList.add(className);
+
+	var tlBar = document.createElement("div");
+	tlBar.classList.add("tl");
+	tlBar.classList.add("bar");
+	tlBar.classList.add(className);
+
+	//add the bar to container
+	newTLContainer.appendChild(tlbkgrd);
+	newTLContainer.appendChild(tlBar);
+
+
+	//now create tl events
+	for(i=0; i<yearArray.length; i++){
+
+		var newEvent = document.createElement("div");
+		newEvent.classList.add("event");
+
+		var yearStandard = ToYearSTR(yearArray[i]);
+		newEvent.classList.add(yearStandard);
+
+		//now add detail container
+		var newDetail = document.createElement("div");
+		newDetail.classList.add("detailcontainer");
+
+		//now add  header
+		var eventHeader = document.createElement("h3");
+   		var header = document.createTextNode(yearArray[i].toString());
+		eventHeader.appendChild(header);
+
+		//now add text
+		var eventTxt = document.createElement("p");
+   		var txt = document.createTextNode(descriptionArray[i]);
+   		eventTxt.appendChild(txt);
+
+   		newDetail.appendChild(eventHeader);
+   		newDetail.appendChild(eventTxt);
+
+		if(evenQ(i)){
+			newDetail.classList.add("top");
+		}else{
+			newDetail.classList.add("bottom");
+		}
+
+		newEvent.appendChild(newDetail);		
+
+		newTLContainer.appendChild(newEvent);
+
+		//now we deend to be able to apply all positions to the global container
+	}
+
+	//finally end by appending our new timeline container into timeline section
+	tlSection.appendChild(newTLContainer);
+
+	rescaleTL(className+"ct" ,1.0);
+}
+
+
+
+
+//var node = document.getElementById('node-id');
+//var newNode = document.createElement('p');
+//newNode.appendChild(document.createTextNode('some dynamic html'));
+//node.appendChild(newNode);
+
+function ToYearSTR(year){
+	var yearstr = year.toString();
+	if( yearstr.startsWith("-") ){
+		yearstr = yearstr.replace("-","bce");
+	}else{
+		yearstr = "ce"+yearstr;
+	}
+
+	return "year"+yearstr;
+}
+
+
+function classNameToYear(className){
+	var nameArray = className.split(" ");
+	//console.log(["classNameToYear: yearstr: ", yearstr]);
+	for(iii=0; iii<nameArray.length; iii++){
+		if( nameArray[iii].startsWith("year") ){
+			var yearstr = nameArray[iii].replace("year","").replace("bce","-").replace("ce","");
+			break;
+		}
+	}
+	console.log(["classNameToYear: yearstr: ", yearstr]);
+	return parseFloat(yearstr);
+
+}
+
+
+//Shift container to center aroun 0 and
 function rescaleTL(cls,scale){
 
 
@@ -14,28 +121,17 @@ function rescaleTL(cls,scale){
 	console.log(["timelines count: ",tlarray.length]);
 
 	for(i=0; i<tlarray.length; i++){
-		var kids = tlarray[i].children;
+
+
 		var tlEvents = tlarray[i].getElementsByClassName("event");
 		var tlDisplay = tlarray[i].getElementsByClassName("tl");
 
 		//for each timeline, get the width and left off set values;
 		//start by getting the event numbers
-		var yearList = [];
-		
+		var yearList = [];		
 		for(ii=0; ii<tlEvents.length; ii++){
-
-			var className = tlEvents[ii].getAttribute("class").split(" ");
-			//console.log(["class name: ", className]);
-
-			for(iii=0; iii<className.length; iii++){
-				if( className[iii].startsWith("year") ){
-					var yearstr = className[iii].replace("year","").replace("bce","-").replace("ce","");
-					//console.log(["yearstr: ", yearstr]);
-					break;
-				}
-			}
-
-			var yearPixNum = Math.round( scale * parseFloat(yearstr) );
+			var yearNum = classNameToYear( tlEvents[ii].getAttribute("class")) ;
+			var yearPixNum = Math.round( scale * yearNum );
 			yearList.push( yearPixNum);
 		}
 
@@ -43,9 +139,6 @@ function rescaleTL(cls,scale){
 		//Now calculate global shift parameters;
 		var shiftLeft = Math.min(...yearList);
 		var width = Math.max(...yearList)- shiftLeft;
-
-		
-
 		//shift the container
 		var ctshift = (-1*shiftLeft).toString()+"px";
 		var tlshift = shiftLeft.toString()+"px";
@@ -53,15 +146,11 @@ function rescaleTL(cls,scale){
 		tlarray[i].style.left = ctshift;//shift container
 
 		for(ii=0; ii<tlDisplay.length; ii++){
-
-			console.log(["initial display left", tlDisplay[ii].getAttribute("style") ]);
-
+			//console.log(["initial display left", tlDisplay[ii].getAttribute("style") ]);
 			tlDisplay[ii].style.left = tlshift;
 			tlDisplay[ii].style.width = width.toString()+"px";
-
-			console.log(["new display left", tlDisplay[ii].getAttribute("style") ]);
+			//console.log(["new display left", tlDisplay[ii].getAttribute("style") ]);
 		}
-
 		//no rescale the events
 		for(ii=0; ii<tlEvents.length; ii++){
 			console.log(["applying shift ",tlEvents[ii],yearList[ii]]);
@@ -72,6 +161,6 @@ function rescaleTL(cls,scale){
 
 function rescaleTLEgypt(){
 	var scale = parseFloat(document.getElementById("scaletl").value);
-	console.log(["rescale egypt",scale, typeof scale]);
+	//console.log(["rescale egypt",scale, typeof scale]);
 	rescaleTL('egyptct',scale);
 }

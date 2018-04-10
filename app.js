6e9035952965c86e2b98610f06f049d43506618b
Kyle Hawkins <ksolohawk@gmail.com>
2018-04-10 11:33:46 -0700

    basic rescaleable timeline

--- app.js
+++ app.js
@@ -18,10 +18,6 @@ function rescaleTL(cls,scale){
 		var tlEvents = tlarray[i].getElementsByClassName("event");
 		var tlDisplay = tlarray[i].getElementsByClassName("tl");
 
-		console.log(["tl: ",i,"children: ",kids.length]);
-		console.log(["tl: ",i,"events: ",tlEvents.length]);
-		console.log(["tl: ",i,"Display Objs: ",tlDisplay.length]);
-
 		//for each timeline, get the width and left off set values;
 		//start by getting the event numbers
 		var yearList = [];
@@ -29,24 +25,53 @@ function rescaleTL(cls,scale){
 		for(ii=0; ii<tlEvents.length; ii++){
 
 			var className = tlEvents[ii].getAttribute("class").split(" ");
-			console.log(["class name: ", className]);
+			//console.log(["class name: ", className]);
 
-			for(iii=0; iii<className.length; i++){
+			for(iii=0; iii<className.length; iii++){
 				if( className[iii].startsWith("year") ){
-					var yearstr = className[iii].valu;
+					var yearstr = className[iii].replace("year","").replace("bce","-").replace("ce","");
+					//console.log(["yearstr: ", yearstr]);
 					break;
 				}
 			}
-			console.log(["yearstr: ", yearstr]);
-			yearList.push(  tlEvents[ii].getAttribute("class")  );
+
+			var yearPixNum = Math.round( scale * parseFloat(yearstr) );
+			yearList.push( yearPixNum);
 		}
 
-		console.log(["tl: ",i,"yearlist: ",yearList]);
+		//console.log(["tl: ",i,"yearlist: ",yearList]);
+		//Now calculate global shift parameters;
+		var shiftLeft = Math.min(...yearList);
+		var width = Math.max(...yearList)- shiftLeft;
 
+		
+
+		//shift the container
+		var ctshift = (-1*shiftLeft).toString()+"px";
+		var tlshift = shiftLeft.toString()+"px";
+
+		tlarray[i].style.left = ctshift;//shift container
+
+		for(ii=0; ii<tlDisplay.length; ii++){
+
+			console.log(["initial display left", tlDisplay[ii].getAttribute("style") ]);
+
+			tlDisplay[ii].style.left = tlshift;
+			tlDisplay[ii].style.width = width.toString()+"px";
+
+			console.log(["new display left", tlDisplay[ii].getAttribute("style") ]);
+		}
+
+		//no rescale the events
+		for(ii=0; ii<tlEvents.length; ii++){
+			console.log(["applying shift ",tlEvents[ii],yearList[ii]]);
+			tlEvents[ii].style.left = yearList[ii].toString()+"px";
+		}
 	}
 }
 
 function rescaleTLEgypt(){
-	var scale;
+	var scale = parseFloat(document.getElementById("scaletl").value);
+	console.log(["rescale egypt",scale, typeof scale]);
 	rescaleTL('egyptct',scale);
 }
